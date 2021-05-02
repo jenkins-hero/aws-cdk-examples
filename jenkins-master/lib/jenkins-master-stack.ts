02fc25cf6e4685cf0d37e9442495b6c11c5fbea9
Tom Gregory <t.k.gregory@gmail.com>
2021-05-02 14:58:18 +0100

    Include route 53 CNAME record

--- jenkins-master/lib/jenkins-master-stack.ts
+++ jenkins-master/lib/jenkins-master-stack.ts
@@ -2,8 +2,9 @@ import * as cdk from '@aws-cdk/core';
 import {Duration} from '@aws-cdk/core';
 import * as ecs from '@aws-cdk/aws-ecs';
 import * as ec2 from '@aws-cdk/aws-ec2';
-import * as secretsmanager from '@aws-cdk/aws-secretsmanager';
 import * as elbv2 from '@aws-cdk/aws-elasticloadbalancingv2';
+import * as route53 from '@aws-cdk/aws-route53';
+import {HostedZone} from '@aws-cdk/aws-route53';
 
 export class JenkinsMasterStack extends cdk.Stack {
     constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -24,19 +25,9 @@ export class JenkinsMasterStack extends cdk.Stack {
             family: 'jenkins'
         });
 
-        const jenkinsPassword = new secretsmanager.Secret(this, 'Secret', {
-            secretName: 'JenkinsPasswordSecret'
-        });
-
         taskDefinition.addContainer('jenkins', {
             image: ecs.ContainerImage.fromRegistry("jenkins/jenkins:lts"),
             logging: ecs.LogDrivers.awsLogs({streamPrefix: 'jenkins'}),
-            environment: {
-                JENKINS_USERNAME: 'developer'
-            },
-            secrets: {
-                JENKINS_PASSWORD: ecs.Secret.fromSecretsManager(jenkinsPassword)
-            },
             portMappings: [{
                 containerPort: 8080
             }]
@@ -51,18 +42,36 @@ export class JenkinsMasterStack extends cdk.Stack {
             healthCheckGracePeriod: Duration.minutes(5)
         });
 
-        const loadBalancer = new elbv2.ApplicationLoadBalancer(this, 'LoadBalancer', {vpc, internetFacing: true});
-        const listener = loadBalancer.addListener('Listener', {
-            port: 443,
-            certificateArns: ['arn:aws:acm:eu-west-1:299404798587:certificate/93b13faf-f41e-4249-91ed-dab5cb78473e']
-        });
-        listener.addTargets('JenkinsTarget', {
-            port: 8080,
-            targets: [service],
-            deregistrationDelay: Duration.seconds(10),
-            healthCheck: {
-                path: '/login'
+        let certificateArn = this.node.tryGetContext('certificateArn');
+        if (certificateArn) {
+            const loadBalancer = new elbv2.ApplicationLoadBalancer(this, 'LoadBalancer', {vpc, internetFacing: true});
+            new cdk.CfnOutput(this, 'LoadBalancerDNSName', {value: loadBalancer.loadBalancerDnsName});
+
+            const listener = loadBalancer.addListener('Listener', {
+                port: 443,
+                certificateArns: [certificateArn]
+            });
+            listener.addTargets('JenkinsTarget', {
+                port: 8080,
+                targets: [service],
+                deregistrationDelay: Duration.seconds(10),
+                healthCheck: {
+                    path: '/login'
+                }
+            });
+
+            const hostedZoneName = this.node.tryGetContext('hostedZoneName')
+            if (hostedZoneName) {
+                const hostedZone = HostedZone.fromLookup(this, 'HostedZone', {
+                    domainName: hostedZoneName
+                });
+                new route53.CnameRecord(this, 'CnameRecord', {
+                    zone: hostedZone,
+                    recordName: 'jenkins',
+                    domainName: loadBalancer.loadBalancerDnsName,
+                    ttl: Duration.minutes(1)
+                });
             }
-        });
+        }
     }
 }
